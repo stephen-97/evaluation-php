61d8b1c4fb0747c0b5a0ffa6e5a8b4096a069885
stephen Loiola Bastos <stephen.loiola@hotmail.com>
2021-04-23 18:29:54 +0200

    mon nom prenom

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 
 /** Page principale, on appele le premier include pour les deux boutons en haut et le deuxième pour la liste de TOUT les articles dans la BBD**/
-
+echo "<h1>Stephen Loiola Bastos</h1>";
 
 include './connect_or_create_template.php';
 include './src/View/all_articles.php';
